09507e070599809573bca3322da8618158755c3a
Steph St <steph.streete@outlook.com>
2021-01-19 17:43:05 +0000

    completed code

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
 #check string length
-
+puts "what is the size of this string?".size
 #convert string into an array
+arr = ['what', 'is', 'the', 'size', 'of', 'this', 'string', '?']
+puts arr.join("_")
